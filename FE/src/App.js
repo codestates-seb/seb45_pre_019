25ab86ce0f7e2f79f9b5289c1312d6bc13f25d5a
sowon_m1 <bsw2377@gmail.com>
2023-08-09 17:09:05 +0900

    [Init] GlobalStyles 생성

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,8 +1,18 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div>
-      <h1>도비즈화이팅!!</h1>
-    </div>
+    <>
+      <GlobalStyles />
+      <h1>도비즈 화이팅!!</h1>
+      <Header />
+      <Sidebar />
+      <Footer />
+    </>
   );
 }
 
---
+++ FE/src/components/Sidebar.js
@@ -0,0 +1,5 @@
+const Sidebar = () => {
+  return <nav>Sidebar</nav>;
+};
+
+export default Sidebar;
