2e037896dae8ff97c296fd16c3615de8bc5b8892
da9dac <da9dac@gmail.com>
2023-08-10 15:42:10 +0900

    [Feat] 회원 엔티티 매핑 및 회원가입 기능 추가 #12

--- BE/src/main/java/com/server/domain/account/service/AccountService.java
+++ BE/src/main/java/com/server/domain/account/service/AccountService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import com.server.domain.account.entity.Account;
 import com.server.domain.account.repository.AccountRepository;
 import com.server.global.exception.advice.BusinessLogicException;
+import com.server.global.exception.exceptionCode.ExceptionCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +21,6 @@ public class AccountService {
 	}
 
 	private void existAccount(Long id) {
-		accountRepository.findById(id).orElseThrow(new BusinessLogicException());
+		accountRepository.findById(id).orElseThrow(() -> new BusinessLogicException(ExceptionCode.NOT_FOUND));
 	}
 }
