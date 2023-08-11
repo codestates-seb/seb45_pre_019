09c57ab6954049da012cb11c72a9ab00be9f764b
김병현 <gimbyeonghyeon@gimbyeonghyeon-ui-MacBookAir.local>
2023-08-11 14:14:29 +0900

    [Feat] 헤더 만들기 #1

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,18 +1,18 @@
-import GlobalStyles from "./styles/GlobalStyles";
-
+import react from "react";
+import styled from "styled-components";
 import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import Footer from "./components/Footer";
+
+const AppContainer = styled.div`
+  font-family: Arial, sans-serif;
+  background-color: #f5f5f5;
+`;
 
 function App() {
   return (
-    <>
-      <GlobalStyles />
-      <h1>도비즈 화이팅!!!</h1>
+    <AppContainer>
       <Header />
-      <Sidebar />
-      <Footer />
-    </>
+      {/* 다른 컴포넌트나 콘텐츠를 추가하세요 */}
+    </AppContainer>
   );
 }
 
