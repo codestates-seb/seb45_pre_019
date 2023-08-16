dad05356185cb0c2ddbc0559613e5d1f37990eec
da9dac <da9dac@gmail.com>
2023-08-16 22:58:58 +0900

    [Feat] 답글 엔티티 매핑 및 기본 틀 구현

---
+++ BE/src/main/java/com/server/domain/reply/controller/ReplyController.java
@@ -0,0 +1,10 @@
+package com.server.domain.reply.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reply")
+public class ReplyController {
+
+}
