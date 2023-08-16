94d24970bfce65c8ecaee5fa76ee981648c2341f
da9dac <da9dac@gmail.com>
2023-08-16 23:33:20 +0900

    Revert "Feature be/reply"

--- BE/src/main/java/com/server/domain/reply/controller/ReplyController.java
+++ BE/src/main/java/com/server/domain/reply/controller/ReplyController.java
@@ -1,14 +1,10 @@
 package com.server.domain.reply.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/reply")
 public class ReplyController {
-	@GetMapping
-	public String test() {
-		return "TeSt!";
-	}
+
 }
