813d6b20879686638b43c6f7de0d08058ec80688
da9dac <da9dac@gmail.com>
2023-08-16 23:06:45 +0900

    [Deploy] 포크 자동 동기화 테스트

--- BE/src/main/java/com/server/domain/reply/controller/ReplyController.java
+++ BE/src/main/java/com/server/domain/reply/controller/ReplyController.java
@@ -1,10 +1,14 @@
 package com.server.domain.reply.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/reply")
 public class ReplyController {
-
+	@GetMapping
+	public String test() {
+		return "TeSt!";
+	}
 }
