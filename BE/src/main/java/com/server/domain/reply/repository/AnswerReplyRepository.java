b462d0d4585447cda0c6426ff8632db76451f8e4
da9dac <da9dac@gmail.com>
2023-08-18 11:06:01 +0900

    [Feat] 답변 엔티티 매핑 및 연관 엔티티 수정

--- BE/src/main/java/com/server/domain/reply/repository/AnswerReplyRepository.java
+++ BE/src/main/java/com/server/domain/reply/repository/AnswerReplyRepository.java
@@ -1,8 +1,8 @@
-// package com.server.domain.reply.repository;
-//
-// import org.springframework.data.jpa.repository.JpaRepository;
-//
-// import com.server.domain.reply.entity.AnswerReply;
-//
-// public interface AnswerReplyRepository extends JpaRepository<AnswerReply, Long> {
-// }
+package com.server.domain.reply.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.server.domain.reply.entity.AnswerReply;
+
+public interface AnswerReplyRepository extends JpaRepository<AnswerReply, Long> {
+}
