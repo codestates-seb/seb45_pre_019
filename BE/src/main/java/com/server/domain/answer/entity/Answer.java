8ea3dd43bf1e92093b701b6d46702395c345b1b1
da9dac <da9dac@gmail.com>
2023-08-18 10:44:26 +0900

    [Feat] 답변 엔티티 매핑 및 연관 엔티티 수정

--- BE/src/main/java/com/server/domain/answer/entity/Answer.java
+++ BE/src/main/java/com/server/domain/answer/entity/Answer.java
@@ -1,4 +1,4 @@
-package com.server.domain.reply.entity;
+package com.server.domain.answer.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
 import com.server.domain.account.entity.Account;
@@ -16,26 +17,28 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity
 @Getter
 @Builder
-@NoArgsConstructor
+@Entity
 @AllArgsConstructor
-public class AnswerReply extends TimeStamp {
+@NoArgsConstructor
+public class Answer extends TimeStamp {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "reply_id")
-	private Long replyId;
+	@Column(name = "answer_id")
+	private Long answerId;
 
-	@Column(name = "reply_content")
-	private String replyContent;
+	@Setter
+	@Column(columnDefinition = "TEXT", nullable = false, name = "answer_content")
+	private String answerContent;
 
 	@ManyToOne
-	@JoinColumn(name = "account_id")
-	private Account account;
+	@JoinColumn(name = "question_Id")
+	private Question question;
 
-	// @ManyToOne
-	// @JoinColumn(name = "answer_id")
-	// private Answer answer;
+	@ManyToOne
+	@JoinColumn(name = "account_Id")
+	private Account account;
 }
