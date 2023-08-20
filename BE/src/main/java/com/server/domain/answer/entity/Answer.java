cac7ddb504af604b5d2fed00a43b975d7afb1639
da9dac <da9dac@gmail.com>
2023-08-20 22:28:09 +0900

    [Fix] 엔티티 최적화

--- BE/src/main/java/com/server/domain/answer/entity/Answer.java
+++ BE/src/main/java/com/server/domain/answer/entity/Answer.java
@@ -10,7 +10,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
