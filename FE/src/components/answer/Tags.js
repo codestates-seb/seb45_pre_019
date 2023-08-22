5090a797c3add77000e73a7c92931cd3289c6680
lynne suyeon choi <choilynne.dev@gmail.com>
2023-08-22 17:09:15 +0900

    [Feat] 태그 UI #53

---
+++ FE/src/components/answer/Tags.js
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+export const Tags = styled.ul`
+  display: inline-flex;
+
+  li {
+    margin-right: 5px;
+    padding: 4px 6px;
+    color: #6b97b7;
+    background-color: #e1ecf4;
+    border-radius: 3px;
+    font-size: 12px;
+  }
+`;
