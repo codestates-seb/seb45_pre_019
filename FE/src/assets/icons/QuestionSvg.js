948145967fbba7a06fdb09143ec1e86dca61e442
lynne suyeon choi <choilynne.dev@gmail.com>
2023-08-22 18:18:48 +0900

    [Feat] Question Leftbar UI #91

---
+++ FE/src/assets/icons/QuestionSvg.js
@@ -0,0 +1,31 @@
+export const ArrowUpSvg = () => {
+  return (
+    <svg aria-hidden="true" width="18" height="18" viewBox="0 0 18 18">
+      <path d="M1 12h16L9 4l-8 8Z"></path>
+    </svg>
+  );
+};
+
+export const ArrowDownSvg = () => {
+  return (
+    <svg aria-hidden="true" width="18" height="18" viewBox="0 0 18 18">
+      <path d="M1 6h16l-8 8-8-8Z"></path>
+    </svg>
+  );
+};
+
+export const BookmarkSvg = () => {
+  return (
+    <svg aria-hidden="true" width="18" height="18" viewBox="0 0 18 18">
+      <path d="m9 10.6 4 2.66V3H5v10.26l4-2.66ZM3 17V3c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2v14l-6-4-6 4Z"></path>
+    </svg>
+  );
+};
+
+export const HistorySvg = () => {
+  return (
+    <svg aria-hidden="true" width="19" height="18" viewBox="0 0 19 18">
+      <path d="M3 9a8 8 0 1 1 3.73 6.77L8.2 14.3A6 6 0 1 0 5 9l3.01-.01-4 4-4-4h3L3 9Zm7-4h1.01L11 9.36l3.22 2.1-.6.93L10 10V5Z"></path>
+    </svg>
+  );
+};
---
+++ FE/src/components/answer/QuestionLeftbar.js
@@ -0,0 +1,65 @@
+import { styled } from "styled-components";
+import {
+  ArrowUpSvg,
+  ArrowDownSvg,
+  BookmarkSvg,
+  HistorySvg,
+} from "../../assets/icons/QuestionSvg";
+
+const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-right: 16px;
+
+  button:nth-child(1) {
+    margin: 2px;
+    padding: 10px;
+    border: 1px solid #dcdfe2;
+    border-radius: 100%;
+  }
+  span {
+    margin: 2px;
+    padding: 4px;
+    font-size: 19px;
+    color: black;
+    border: none;
+  }
+  button:nth-child(3) {
+    margin: 2px 2px 8px 2px;
+    padding: 10px;
+    border: 1px solid #dcdfe2;
+    border-radius: 100%;
+  }
+  button:nth-child(4) {
+    padding: 4px 0;
+    border: none;
+    fill: #bbc0c5;
+  }
+  button:nth-child(5) {
+    margin: 2px 14px;
+    padding: 6px 0;
+    border: none;
+    fill: #bbc0c5;
+  }
+`;
+
+export const QuestionLeftBar = () => {
+  return (
+    <Block>
+      <button>
+        <ArrowUpSvg />
+      </button>
+      <span>1</span>
+      <button>
+        <ArrowDownSvg />
+      </button>
+      <button>
+        <BookmarkSvg />
+      </button>
+      <button>
+        <HistorySvg />
+      </button>
+    </Block>
+  );
+};
