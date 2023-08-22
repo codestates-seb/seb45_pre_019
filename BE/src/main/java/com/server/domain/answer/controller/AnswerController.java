c6092d662093ccc9613fe044a0290b5436df2fc1
da9dac <da9dac@gmail.com>
2023-08-22 23:09:51 +0900

    [Feat] 답변 API 문서화

--- BE/src/main/java/com/server/domain/answer/controller/AnswerController.java
+++ BE/src/main/java/com/server/domain/answer/controller/AnswerController.java
@@ -24,11 +24,16 @@ import com.server.domain.answer.service.AnswerService;
 import com.server.global.argumentsresolver.LoginAccountIdResolver;
 import com.server.global.common.dto.SingleResDto;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/questions")
+@Api(tags = {"3. 답변 관리"}, description = "답변 CRUD 관련 서비스")
 public class AnswerController {
 	private final static String ANSWER_DEFAULT_URL = "/questions";
 	private final AnswerMapper mapper;
@@ -36,6 +41,15 @@ public class AnswerController {
 
 
 	@PostMapping("/{question-id}/post")
+	@ApiOperation(value = "답변 등록", notes = "새로운 답변을 등록하는 API")
+	@ApiResponses(
+		{
+			@ApiResponse(code = 400, message = "질문을 찾을 수 없습니다."),
+			@ApiResponse(code = 401, message = "인증되지 않은 사용자입니다."),
+			@ApiResponse(code = 403, message = "권한이 없는 사용자입니다."),
+			@ApiResponse(code = 201, message = "CREATED")
+		}
+	)
 	public ResponseEntity postAnswer(@RequestBody AnswerDto.Post postDto,
 									  @Positive @PathVariable("question-id") Long questionId) {
 		postDto.addAccountId(LoginAccountIdResolver.getAccountId());
@@ -48,6 +62,15 @@ public class AnswerController {
 	}
 
 	@PatchMapping("/update/{question-id}/{answer-id}")
+	@ApiOperation(value = "답변 수정", notes = "기존 답변을 수정하는 API")
+	@ApiResponses(
+		{
+			@ApiResponse(code = 400, message = "질문을 찾을 수 없습니다."),
+			@ApiResponse(code = 401, message = "인증되지 않은 사용자입니다."),
+			@ApiResponse(code = 403, message = "권한이 없는 사용자입니다."),
+			@ApiResponse(code = 201, message = "OK")
+		}
+	)
 	public ResponseEntity<SingleResDto<String>> patchAnswer(@RequestBody AnswerDto.Patch patchDto,
 										@PathVariable("answer-id") Long answerId,
 										@PathVariable("question-id") Long questionId) {
@@ -62,6 +85,13 @@ public class AnswerController {
 	}
 
 	@GetMapping("/{question-id}/answerList")
+	@ApiOperation(value = "답변 전체 조회", notes = "질문에 해당하는 모든 답변을 조회하는 API")
+	@ApiResponses(
+		{
+			@ApiResponse(code = 400, message = "질문을 찾을 수 없습니다."),
+			@ApiResponse(code = 201, message = "OK")
+		}
+	)
 	public ResponseEntity getAnswers(@PathVariable("question-id") Long questionId) {
 		List<Answer> answers = answerService.findAnswers(questionId);
 
@@ -69,6 +99,16 @@ public class AnswerController {
 	}
 
 	@DeleteMapping("/{answer-id}")
+	@ApiOperation(value = "답변 삭제", notes = "기존 답변을 삭제하는 API")
+	@ApiResponses(
+		{
+			@ApiResponse(code = 400, message = "질문을 찾을 수 없습니다."),
+			@ApiResponse(code = 400, message = "삭제 권한이 없습니다."),
+			@ApiResponse(code = 401, message = "인증되지 않은 사용자입니다."),
+			@ApiResponse(code = 403, message = "권한이 없는 사용자입니다."),
+			@ApiResponse(code = 201, message = "OK")
+		}
+	)
 	public ResponseEntity deleteAnswer(@PathVariable("answer-id") Long answerId) {
 		Long loginAccountId = LoginAccountIdResolver.getAccountId();
 		answerService.deleteAnswer(answerId, loginAccountId);
