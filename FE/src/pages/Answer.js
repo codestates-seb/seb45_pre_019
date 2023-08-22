1e026ace2c6b8ae75c1e2aee0e3bf292312c3436
lynne suyeon choi <choilynne.dev@gmail.com>
2023-08-23 01:44:13 +0900

    Answer: 페이지 완성 #91

---
+++ FE/src/pages/Answer.js
@@ -0,0 +1,25 @@
+import { styled } from "styled-components";
+import { QuestionHeader } from "../components/answer/QuestionHeader";
+import { QuestionTemplate } from "../components/answer/QuestionTemplate";
+import { Answers } from "../components/answer/Answers";
+
+const Container = styled.div`
+  /* 좌우 모서리와 간격 82px 컨테이너 */
+  width: calc(100% - 164px);
+  max-width: 1100px;
+  padding: 24px;
+`;
+
+const Content = styled.div``;
+
+export const AnswerPage = () => {
+  return (
+    <Container>
+      <QuestionHeader />
+      <Content>
+        <QuestionTemplate type="question" />
+        <Answers />
+      </Content>
+    </Container>
+  );
+};
